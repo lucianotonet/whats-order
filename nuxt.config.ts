8aaf713e682a164736eda5c1d3581b98adb166be
Luciano T. <tonetlds@gmail.com>
2024-08-28 21:50:40 -0300

    Adiciona layout e configurações de fontes usando Roboto
    Implementa um novo layout com suporte ao Nuxt, além de integrar a fonte Roboto para uma melhor experiência visual. O código inclui elementos de configuração e novas páginas, substituindo a página de administração removida. Elimina a complexidade do gerenciamento anterior dos uploads de clientes e produtos, integrando funcionalidades em uma nova página de configurações e simplificando o fluxo de dados. A mudança melhora a estética e a funcionalidade da aplicação, promovendo uma interface mais coesa e moderna.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,15 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     head: {
-      script: [{src: 'https://cdn.tailwindcss.com'}]
+      script: [{src: 'https://cdn.tailwindcss.com'}],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', rel: 'stylesheet' }
+      ]
     }
   },
   modules: [
     '@pinia/nuxt',
   ],
-  // Remova a configuração do Nitro storage
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,59 @@
 <template>
     <div class="mx-auto px-12 md:px-24 w-full py-6 md:py-12">
         <div class="max-w-2xl mx-auto">
-            <h1 class="text-2xl font-black mb-8">SOLICITAÇÃO DE OFERTA 🎯 <small>v2.0</small></h1>
 
-            <!-- Informações rápidas -->
-            <div class="bg-gray-100 p-4 rounded-lg mb-8">
-                <h2 class="text-xl font-bold mb-2">Informações do Sistema</h2>
-                <p>Total de Clientes: {{ store.totalClientes || 0 }}</p>
-                <p>Total de Produtos: {{ store.totalProdutos || 0 }}</p>
+            <div class="flex justify-between items-center mb-12">
+                <h1 class="text-2xl font-black">SOLICITAÇÃO DE OFERTA 🎯 <small>v3.0</small></h1>
             </div>
 
             <form @submit.prevent="sendOrder" class="grid gap-y-4">
                 <div class="flex flex-col gap-4 md:flex-row">
                     <div class="flex flex-col w-full">
                         <label for="rca">RCA</label>
-                        <input required v-model="rca" class="w-full border border-black px-2 py-1" type="text"
-                            name="rca" id="rca">
+                        <input required v-model="rca" class="w-full rounded shadow-inner border border-black px-2 py-1"
+                            type="text" name="rca" id="rca">
                     </div>
                     <div class="flex flex-col w-full">
                         <label for="precoAutorizado">Preço Autorizado</label>
-                        <input required v-model="precoAutorizado" class="w-full border border-black px-2 py-1"
-                            type="text" name="precoAutorizado" id="precoAutorizado">
+                        <input required v-model="precoAutorizado"
+                            class="w-full rounded shadow-inner border border-black px-2 py-1" type="text"
+                            name="precoAutorizado" id="precoAutorizado">
                     </div>
                 </div>
 
                 <div class="flex flex-col gap-4 md:flex-row">
                     <div class="flex flex-col w-full md:w-1/3">
                         <label for="codigoProduto">Código do Produto</label>
-                        <input required v-model="codigoProduto" class="border border-black px-2 py-1" type="text"
-                            name="codigoProduto" id="codigoProduto" @input="updateDescricaoProduto" list="produtoList">
+                        <input required v-model="codigoProduto"
+                            class="rounded shadow-inner border border-black px-2 py-1" type="text" name="codigoProduto"
+                            id="codigoProduto" @input="handleProdutoInput; updateDescricaoProduto()" list="produtoList">
                         <datalist id="produtoList">
-                            <option v-for="produto in store.produtos" :key="produto.codigo" :value="produto.codigo" />
+                            <option v-for="produto in produtos" :key="produto.codigo" :value="produto.codigo" />
                         </datalist>
                     </div>
                     <div class="flex flex-col w-full md:w-2/3">
                         <label for="descricaoProduto">Descrição do Produto</label>
                         <input required v-model="descricaoProduto"
-                            class="w-full border border-black px-2 py-1 bg-gray-200" type="text" name="descricaoProduto"
-                            id="descricaoProduto" readonly>
+                            class="w-full rounded shadow-inner border border-black px-2 py-1 bg-gray-200" type="text"
+                            name="descricaoProduto" id="descricaoProduto" readonly>
                     </div>
                 </div>
 
                 <div class="flex flex-col gap-4 md:flex-row">
                     <div class="flex flex-col w-full md:w-1/3">
                         <label for="codigoCliente">Código do Cliente</label>
-                        <input required v-model="codigoCliente" class="border border-black px-2 py-1" type="text"
-                            name="codigoCliente" id="codigoCliente" @input="updateRazaoSocialCliente"
+                        <input required v-model="codigoCliente"
+                            class="rounded shadow-inner border border-black px-2 py-1" type="text" name="codigoCliente"
+                            id="codigoCliente" @input="handleClienteInput; updateRazaoSocialCliente()"
                             list="clienteList">
                         <datalist id="clienteList">
-                            <option v-for="cliente in store.clientes" :key="cliente.codigo" :value="cliente.codigo" />
+                            <option v-for="cliente in clientes" :key="cliente.codigo" :value="cliente.codigo" />
                         </datalist>
                     </div>
                     <div class="flex flex-col w-full md:w-2/3">
                         <label for="razaoSocialCliente">Razão Social do Cliente</label>
                         <input required v-model="razaoSocialCliente"
-                            class="w-full border border-black px-2 py-1 bg-gray-200" type="text"
+                            class="w-full rounded shadow-inner border border-black px-2 py-1 bg-gray-200" type="text"
                             name="razaoSocialCliente" id="razaoSocialCliente" readonly>
                     </div>
                 </div>
@@ -62,24 +61,28 @@
                 <div class="flex flex-col gap-4 md:flex-row">
                     <div class="flex flex-col w-full md:w-1/3">
                         <label for="quantidade">Quantidade</label>
-                        <input required v-model="quantidade" class="w-full border border-black px-2 py-1"
-                            type="text" name="quantidade" id="quantidade">
+                        <input required v-model="quantidade"
+                            class="w-full rounded shadow-inner border border-black px-2 py-1" type="text"
+                            name="quantidade" id="quantidade">
                     </div>
                     <div class="flex flex-col w-full md:w-1/3">
                         <label for="precoTablet">Preço Tablet</label>
-                        <input required v-model="precoTablet" class="w-full border border-black px-2 py-1"
-                            type="text" name="precoTablet" id="precoTablet">
+                        <input required v-model="precoTablet"
+                            class="w-full rounded shadow-inner border border-black px-2 py-1" type="text"
+                            name="precoTablet" id="precoTablet">
                     </div>
                     <div class="flex flex-col w-full md:w-1/3">
                         <label for="flexRca">FLEX/RCA</label>
-                        <input required v-model="flexRca" class="w-full border border-black px-2 py-1" type="text"
-                            name="flexRca" id="flexRca">
+                        <input required v-model="flexRca"
+                            class="w-full rounded shadow-inner border border-black px-2 py-1" type="text" name="flexRca"
+                            id="flexRca">
                     </div>
                 </div>
-                <button type="submit" :class="{
-                    'opacity-20 disabled cursor-not-allowed': !isValid,
-                    'opacity-100': !!isValid
-                }" class="w-full bg-black text-white py-2 px-3">Enviar</button>
+
+                <button type="submit"
+                    :class="{ 'opacity-20 disabled cursor-not-allowed': !isValid, 'opacity-100': !!isValid }"
+                    class="w-full bg-black text-white py-2 px-3 rounded shadow mt-6">Enviar</button>
+
             </form>
         </div>
     </div>
@@ -100,40 +103,66 @@ const codigoCliente = ref(null);
 const quantidade = ref(null);
 const precoTablet = ref(null);
 const flexRca = ref(null);
+const produtos = ref([]);
+const clientes = ref([]);
+const searchTermProduto = ref('');
+const searchTermCliente = ref('');
+
+const loadProdutos = async (term) => {
+    const response = await fetch('/produtos.json');
+    const allProdutos = await response.json();
+    produtos.value = allProdutos.filter(produto => produto.descricao.toLowerCase().includes(term.toLowerCase()));
+};
+
+const loadClientes = async (term) => {
+    const response = await fetch('/clientes.json');
+    const allClientes = await response.json();
+    clientes.value = allClientes.filter(cliente => cliente.razaoSocial.toLowerCase().includes(term.toLowerCase()));
+};
 
 onMounted(async () => {
-    await store.loadClientes();
-    await store.loadProdutos();
+    await loadClientes('');
+    await loadProdutos('');
 });
 
 const updateDescricaoProduto = () => {
-    const produto = store.produtos.find(p => p.codigo.toString() === codigoProduto.value);
+    const produto = produtos.value.find(p => p.codigo.toString() === codigoProduto.value);
     descricaoProduto.value = produto ? produto.descricao : '';
 };
 
 const updateRazaoSocialCliente = () => {
-    const cliente = store.clientes.find(c => c.codigo.toString() === codigoCliente.value);
+    const cliente = clientes.value.find(c => c.codigo.toString() === codigoCliente.value);
     razaoSocialCliente.value = cliente ? cliente.razaoSocial : '';
+};
+
+const handleProdutoInput = () => {
+    if (searchTermProduto.value.length > 2) {
+        loadProdutos(searchTermProduto.value);
+    }
+};
+
+const handleClienteInput = () => {
+    if (searchTermCliente.value.length > 2) {
+        loadClientes(searchTermCliente.value);
+    }
 };
 
 const whatsAppLink = computed(() => {
     let br = "%0A";
 
-    let message = `
-⛔ *SOLICITAÇÃO DE OFERTA* 🎯${br} 
-${br} 
-● *RCA*:  ${rca.value ?? '-'}${br} 
-● *Preço Autorizado*:  ${precoAutorizado.value ?? '-'}${br} 
-● *Descrição produto*:  ${descricaoProduto.value ?? '-'}${br} 
-● *Código produto*:  ${codigoProduto.value ?? '-'}${br} 
-● *Razão Social cliente*:  ${razaoSocialCliente.value ?? '-'}${br} 
-● *Código do cliente*:  ${codigoCliente.value ?? '-'}${br} 
-● *Quantidade*:  ${quantidade.value ?? '-'}${br} 
-● *Preço Tablet*:  ${precoTablet.value ?? '-'}${br} 
-● *FLEX/RCA*:  ${flexRca.value ?? '-'}${br}
-`;
+    let message = `*SOLICITAÇÃO DE OFERTA* ${br}
+${br}● *RCA*:  ${rca.value ?? '-'}
+${br}● *Preço Autorizado*:  ${precoAutorizado.value ?? '-'}
+${br}● *Código produto*:  ${codigoProduto.value ?? '-'}
+${br}● *Descrição produto*:  ${descricaoProduto.value ?? '-'}
+${br}● *Código do cliente*:  ${codigoCliente.value ?? '-'}
+${br}● *Razão Social cliente*:  ${razaoSocialCliente.value ?? '-'}
+${br}● *Quantidade*:  ${quantidade.value ?? '-'}
+${br}● *Preço Tablet*:  ${precoTablet.value ?? '-'}
+${br}● *FLEX/RCA*:  ${flexRca.value ?? '-'}`;
 
-    return `https://api.whatsapp.com/send?phone=5554999256258&text=${message}%0A`;
+    // return `https://api.whatsapp.com/send?phone=5554999256258&text=${message}%0A`;
+    return `https://api.whatsapp.com/send?phone=5554996132844&text=${message}`;
 });
 
 const isValid = computed(() => {
