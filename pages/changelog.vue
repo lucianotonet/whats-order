d95a978d07fa173d100e09363543e7d59bc3e664
Luciano T. <tonetlds@gmail.com>
2024-09-09 13:51:46 -0300

    Atualiza formato de exibição do changelog e versão
    Melhora a apresentação do changelog ao dividir o conteúdo em seções e parágrafos, proporcionando uma experiência de leitura mais clara. A adição de quebras de linha facilita a visualização do conteúdo formatado. Além disso, a versão do aplicativo foi atualizada para refletir as mudanças recentes.

--- pages/changelog.vue
+++ pages/changelog.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold mb-6">Changelog</h1>
-    <div v-html="changelogContent" class="prose prose-sm max-w-none"></div>
+  <div class="container max-w-3xl mx-auto px-4 py-8">
+    <div v-html="formattedChangelogContent" class="prose prose-sm max-w-none"></div>
   </div>
 </template>
 
@@ -10,15 +9,20 @@ import { ref, onMounted } from 'vue';
 import { marked } from 'marked';
 
 const changelogContent = ref('');
+const formattedChangelogContent = ref('');
 
 onMounted(async () => {
   try {
     const response = await fetch('/CHANGELOG.md');
     const markdown = await response.text();
-    changelogContent.value = marked(markdown);
+    const sections = markdown.split('---'); // Divide o conteúdo em seções
+    formattedChangelogContent.value = sections.map(section => {
+      const paragraphs = section.split('. ').map(paragraph => paragraph.trim()).filter(paragraph => paragraph); // Divide em parágrafos
+      return marked(paragraphs.join('.<br>')); // Converte cada parágrafo e junta com nova linha
+    }).join('<br>'); // Converte cada seção e junta
   } catch (error) {
     console.error('Erro ao carregar o changelog:', error);
-    changelogContent.value = '<p>Erro ao carregar o changelog.</p>';
+    formattedChangelogContent.value = '<p>Erro ao carregar o changelog.</p>';
   }
 });
 </script>
